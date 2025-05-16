11105814674ff681a9ce3e279da9ea92d18ecdee
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-05-16 17:27:24 +0100

    update ffile

---
+++ Js/index.js
@@ -0,0 +1,15 @@
+// var chideozieChukwu = "Is a Man"
+// console.log(chideozieChukwu)
+
+// var chideozieChukwu = "is a woman"
+// console.log(chideozieChukwu)
+
+let chinonSo ="Is a boy"
+// console.log(chinoNso)
+
+let chinoNso ="Is a Girl"
+console.log(chinoNso)
+console.log(chinonSo)
+console.log(chinoNso)
+
+const 
---
+++ readme.md
@@ -0,0 +1,10 @@
+# This is a Tattoo Parlour Website
+
+## This project show cases my Learning Path
+I started this project while learning with [freecodecamp](https://freecodecamp.org) full stack playlist on youtube featuring Dave Gray.
+
+### Tech Stack
+    - html5
+    - CSS3
+    - Tailwind CSS
+    - Javascript
